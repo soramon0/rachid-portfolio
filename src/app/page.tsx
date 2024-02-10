bb1dfb92a2d458e84b094f9bff8adda7c9285c0c
soramon0 <23435312+soramon0@users.noreply.github.com>
2024-02-10 21:47:20 +0100

    feat: responsive coming soon header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,10 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between bg-primary p-24">
-      <h1 className="text-4xl text-primary-foreground">Coming Soon</h1>
+    <main className="flex min-h-screen flex-col items-center justify-center bg-primary p-4">
+      <h1 className="text-3xl text-primary-foreground sm:text-4xl">
+        Coming Soon
+      </h1>
     </main>
   );
 }
