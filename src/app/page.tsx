72b44b11ef3078d6fc4fbe764dad991b622f3743
soramon0 <23435312+soramon0@users.noreply.github.com>
2024-02-11 13:44:24 +0100

    feat: add favicon, system colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,7 @@
-import Image from 'next/image';
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-primary p-4">
-      <h1 className="text-3xl text-primary-foreground sm:text-4xl">
-        Coming Soon
-      </h1>
+    <main className="flex min-h-screen flex-col items-center justify-center p-4">
+      <h1 className="text-3xl sm:text-4xl">Coming Soon</h1>
     </main>
   );
 }
